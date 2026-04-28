2744310b27e2c6c9f465b754984d070173b7fb43
sanmaykant <sanmaykant1@gmail.com>
2026-04-28 14:13:04 +0530

    Done

--- src/main/java/com/library/management_system/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.library.management_system.repository;
-
-import com.library.management_system.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book, Long> {}
